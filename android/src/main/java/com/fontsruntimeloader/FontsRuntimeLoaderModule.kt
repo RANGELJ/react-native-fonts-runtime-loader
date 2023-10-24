b6a2dc0833a61d9794fc0008f2bd4304af4a7df2
Jorge Rangel <jorgerangel@192.168.1.6>
2023-10-24 13:10:40 -0600

    refactor: add base to create typeface and return it

--- android/src/main/java/com/fontsruntimeloader/FontsRuntimeLoaderModule.kt
+++ android/src/main/java/com/fontsruntimeloader/FontsRuntimeLoaderModule.kt
@@ -5,7 +5,9 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.Arguments
 import java.io.File
+import android.graphics.Typeface
 
 class FontsRuntimeLoaderModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -27,7 +29,20 @@ class FontsRuntimeLoaderModule(reactContext: ReactApplicationContext) :
       return
     }
 
-    promise.resolve(filePath)
+    val typeface: Typeface
+    try {
+        typeface = Typeface.createFromFile(filePath)
+    } catch (e: Exception) {
+        promise.reject("TYPEFACE_CREATION_FAILED", "Failed to create typeface from file.")
+        return
+    }
+
+    val typefaceData = Arguments.createMap()
+    typefaceData.putInt("style", typeface.style)
+    typefaceData.putBoolean("isBold", typeface.isBold)
+    typefaceData.putBoolean("isItalic", typeface.isItalic)
+
+    promise.resolve(typefaceData)
   }
 
   companion object {
